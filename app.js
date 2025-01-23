6d1f9341e2b10bf33b825ed6d5645df729801a93
arjunpswork40 <arjunpswork40@gmail.com>
2025-01-24 00:33:27 +0530

    ss

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser")
 const multer = require("multer");
 const { PORT } = process.env
 const { makeJsonResponse } = require("./utils/response");
+const path = require('path');
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
